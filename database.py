d037261cf6c49fa671e74dae7436aad161193d86
Ron <ron@Stubbs.(none)>
2010-11-26 13:23:15 -0800

    Pre sqlobject implementation commit.

--- database.py
+++ database.py
@@ -25,36 +25,64 @@ class MySQLDatabase(baseobject.BaseObject):
         return True
 
 class SQLiteDatabase(baseobject.BaseObject):
-    _classAttrs = { "_conn"        : None,
-                    "path" : "./database.db", }
+    _classAttrs = { "_conn" : None,
+                    "path"  : "./openeq.database", }
     _datatypeMap = { "boolean" : "BOOL",
                      "int"     : "INTEGER",
-                     "varchar" : "TEXT", }
+                     "varchar" : "VARCHAR", }
     _type = "sqlite3"
 
 
     def _buildCreateColumnSyntax(this, name = "", columns = {}):
-        rval = "%s %s" %( name, this._datatypeMap[ columns["type"] ] )
-        if columns["primary"].lower() == "true":
+        rval = "\"%s\" %s" %( name, this._datatypeMap[ columns["type"] ] )
+        if columns.has_key("primary") and \
+           columns["primary"].lower() == "true":
             rval += " PRIMARY KEY"
+        if columns.has_key("auto-increment") and \
+           columns["auto-increment"].lower() == "true":
+            rval += " AUTOINCREMENT"
+        if columns.has_key("null") and \
+           columns["null"] == "false":
+            rval += " NOT NULL"
+        if columns.has_key("unique") and \
+           columns["unique"].lower() == "true":
+            rval += " UNIQUE"
         return rval
 
-    def _createTable(this, name = "", columns = {}):
-        cmd = "CREATE TABLE %s (" %name
+    def _createTable(this, name = "", columns = {}, dropFirst = True):
+        if dropFirst:
+            this._execute("DROP TABLE IF EXISTS \"%s\";\n" %name)
+        cmd = "CREATE TABLE \"%s\" (" %name
         for column in columns.items():
             cmd += this._buildCreateColumnSyntax(column[0], column[1])
             cmd += ",\n"
-        cmd += ")"
-        print cmd
+        cmd += ");\n"
+        print this._execute(cmd)
 
     def _connect(this):
-        if this != None:
+        if this._conn != None:
             return
-        this._conn = sqlite3.connect(this.databasePath)
+        this._conn = sqlite3.connect(this.path)
 
     def _disconnect(this):
+        if this._conn == None:
+            return
         this._conn.close()
         this._conn = None
+
+    def _execute(this, cmd):
+        results = None
+        this._connect()
+        try:
+            results = this._conn.execute(cmd)
+        except sqlite3.OperationalError as err:
+            print "-" * 80
+            print "ERROR: Execute command failed!!"
+            print "%s" %cmd
+            print "\n\n%s\n" %str(err)
+            print "-" * 80
+        this._disconnect()
+        return results
 
     def get(this, indices = []):
         return []
--- users.sql
+++ users.sql
@@ -1,2 +1,9 @@
 DROP TABLE IF EXISTS "users";
-CREATE TABLE "users" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "username" VARCHAR NOT NULL  UNIQUE , "password" VARCHAR NOT NULL , "email" VARCHAR NOT NULL  UNIQUE , "enabled" BOOL NOT NULL , "cansubmit" BOOL NOT NULL , "canapprove" BOOL NOT NULL );
+CREATE TABLE "users" (
+    "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , 
+    "username" VARCHAR NOT NULL  UNIQUE ,
+    "password" VARCHAR NOT NULL ,
+    "email" VARCHAR NOT NULL  UNIQUE , 
+    "enabled" BOOL NOT NULL , 
+    "cansubmit" BOOL NOT NULL ,
+    "canapprove" BOOL NOT NULL );
